4e811734ad31781254e2fe648806c67c49e76f16
Steven Shao <stevenshao1900@icloud.com>
2018-09-09 10:29:48 +0800

    Initial Commit

---
+++ MiniNumInSpinningArray/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiniNumInSpinningArray
+//
+//  Created by Steven Shao on 09/09/2018.
+//  Copyright © 2018 Steven Shao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
